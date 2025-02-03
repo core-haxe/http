2ee560cd5b2b451f6222799872726826c949cc3d
Ian Harrigan <ianharrigan@hotmail.com>
2025-02-03 17:19:41 +0100

    added content type

--- src/http/ContentTypes.hx
+++ src/http/ContentTypes.hx
@@ -3,4 +3,5 @@ package http;
 enum abstract ContentTypes(String) from String to String {
     var ApplicationJson = "application/json";
     var TextPlain = "text/pain";
+    var FormUrlEncoded = "application/x-www-form-urlencoded";
 }
